22cbe18284beca9374c112ea7cc3f9fda7b406b3
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-08-13 10:05:09 -0300

    ultima att
    ultima att

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -43,13 +43,15 @@ export default function MobileFilters({
                 <div className="fixed inset-0 bg-black bg-opacity-25" />
 
                 {/* DIalog position */}
-                <div className="fixed insert-0 z-40 flex">
+                <div className="fixed inset-0 z-40 flex">
                     <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
 
                         {/* Close button */}
                         <div className="flex items-center justify-end px-4">
                             <IconButton icon={<X size={15} />} onClick={onClose} />
                         </div>
+
+                        {/* Render the filters */}
 
                         <div className="p-4">
                             <Filter
