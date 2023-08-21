ffa1e41a0205d438ffe373c1f9828f6f00d86931
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-08-21 14:02:28 -0300

    FInal
    Projeto final

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -32,6 +32,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
             )}
             ref={ref}
             {...props}
+            disabled={disabled}
         >
             {children}
         </button>
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,14 +8,15 @@ import { Expand, ShoppingCart } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { MouseEventHandler } from "react"
 import usePreviewModal from "@/hooks/use-preview-modal"
+import useCart from "@/hooks/use-cart"
 
 interface ProductCardProps {
     data: Product
 }
 
 export default function ProductCard({ data }: ProductCardProps) {
+    const cart = useCart()
     const previewModal = usePreviewModal()
-
     const router = useRouter()
     const handleClick = () => {
         router.push(`/product/${data?.id}`)
@@ -25,6 +26,11 @@ export default function ProductCard({ data }: ProductCardProps) {
         event.stopPropagation()
 
         previewModal.onOpen(data)
+    }
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation()
+
+        cart.addItem(data)
     }
 
 
@@ -45,7 +51,7 @@ export default function ProductCard({ data }: ProductCardProps) {
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
-                            onClick={() => { }}
+                            onClick={onAddToCart}
                             icon={<ShoppingCart size={20} className="text-gray-600" />}
                         />
 
